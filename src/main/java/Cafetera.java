32d129cf7e0bf10983bb0c723af3a47ed4436788
ZoeAcero <zar.amazona@gmail.com>
2024-09-24 15:49:47 +0200

    a

---
+++ src/main/java/Cafetera.java
@@ -0,0 +1,55 @@
+public class Cafetera {
+    private int capacidadMaxima;
+    private int cantidadActual;
+
+    
+    public Cafetera() {
+        this.capacidadMaxima = 1000;
+        this.cantidadActual = 0;
+    }
+
+
+    public Cafetera(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+        this.cantidadActual = capacidadMaxima;
+    }
+
+
+    public Cafetera(int capacidadMaxima, int cantidadActual) {
+        this.capacidadMaxima = capacidadMaxima;
+        if (cantidadActual > capacidadMaxima) {
+            this.cantidadActual = capacidadMaxima;
+        } else {
+            this.cantidadActual = cantidadActual;
+        }
+    }
+
+
+    public void llenarCafetera() {
+        this.cantidadActual = this.capacidadMaxima;
+    }
+
+    public void servirTaza(int cantidad) {
+        if (cantidad > this.cantidadActual) {
+            this.cantidadActual = 0;
+        } else {
+            this.cantidadActual -= cantidad;
+        }
+    }
+
+    public void vaciarCafetera() {
+        this.cantidadActual = 0;
+    }
+
+    public void agregarCafe(int cantidad) {
+        if (this.cantidadActual + cantidad > this.capacidadMaxima) {
+            this.cantidadActual = this.capacidadMaxima;
+        } else {
+            this.cantidadActual += cantidad;
+        }
+    }
+
+    public int getCantidadActual() {
+        return this.cantidadActual;
+    }
+}
